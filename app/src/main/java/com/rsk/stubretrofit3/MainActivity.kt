0e9dbaf5dcd63fb3323a501b5b25ccc3c8fa1db5
collinsturi <cmachuka70@gmail.com>
2022-11-12 23:22:53 +0300

    Recycler view adapter and setting up the recycler view on Main activity

--- app/src/main/java/com/rsk/stubretrofit3/MainActivity.kt
+++ app/src/main/java/com/rsk/stubretrofit3/MainActivity.kt
@@ -2,10 +2,28 @@ package com.rsk.stubretrofit3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Lifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.rsk.stubretrofit3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var postsAdapter: PostsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUpRecyclerView()
+
+
+    }
+
+    private fun setUpRecyclerView() = binding.rvPosts.apply {
+            postsAdapter = PostsAdapter()
+            adapter = postsAdapter
+            layoutManager = LinearLayoutManager(this@MainActivity)
     }
 }
--- app/src/main/java/com/rsk/stubretrofit3/PostsAdapter.kt
+++ app/src/main/java/com/rsk/stubretrofit3/PostsAdapter.kt
@@ -1,4 +1,42 @@
 package com.rsk.stubretrofit3
 
-class PostsAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.rsk.stubretrofit3.databinding.ItemPostsBinding
+
+class PostsAdapter : RecyclerView.Adapter<PostsAdapter.PostsViewHolder>() {
+    inner class PostsViewHolder(val binding: ItemPostsBinding) : RecyclerView.ViewHolder(binding.root)
+
+    private val diffCallback = object : DiffUtil.ItemCallback<Posts>(){
+        override fun areItemsTheSame(oldItem: Posts, newItem: Posts): Boolean = (oldItem.userId == oldItem.userId)
+
+        override fun areContentsTheSame(oldItem: Posts, newItem: Posts): Boolean = (oldItem == newItem)
+    }
+
+    private val differ = AsyncListDiffer<Posts>(this, diffCallback)
+
+    var posts: List<Posts>
+        get() = differ.currentList
+        set(value) { differ.submitList(value)}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostsViewHolder {
+        return PostsViewHolder(ItemPostsBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false))
+    }
+
+    override fun getItemCount(): Int = posts.size
+
+    override fun onBindViewHolder(holder: PostsViewHolder, position: Int) {
+        holder.binding.apply {
+            val item = posts[position]
+
+            tvTitle.text = item.title
+            tvBody.text = item.body
+        }
+    }
 }
